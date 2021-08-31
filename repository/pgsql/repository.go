01e5066e6434c6eab24af4a88dee6de6109ba903
ediprako <ediprakoso@gmail.com>
2021-08-31 11:44:09 +0700

    add status to product_images add mechanism insert image so same image not inserted twice

--- repository/pgsql/repository.go
+++ repository/pgsql/repository.go
@@ -2,6 +2,7 @@ package pgsql
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -42,6 +43,17 @@ type PriceHistory struct {
 	OriginalPrice int64     `db:"original_price"`
 	UpdateTime    time.Time `db:"updated_at"`
 }
+
+type ProductImage struct {
+	ID        int64  `db:"id"`
+	ProductID int64  `db:"product_id"`
+	Image     string `db:"image"`
+}
+
+const (
+	stateActive  = 1
+	stateDeleted = 0
+)
 
 func (r *repository) GetProductsByID(ctx context.Context, id int64) (Product, error) {
 	sql := `SELECT id, name, current_price, original_price,coalesce(url,'') url FROM
@@ -84,8 +96,8 @@ func (r *repository) GetProducts(ctx context.Context, limit, offset int) ([]Prod
 		listProductID[i] = product.ID
 	}
 
-	sqlImage := `SELECT product_id,image FROM product_images WHERE product_id = ANY($1)`
-	rows, err := r.db.QueryxContext(ctx, sqlImage, pq.Array(listProductID))
+	sqlImage := `SELECT product_id,image FROM product_images WHERE product_id = ANY($1) and status=$2`
+	rows, err := r.db.QueryxContext(ctx, sqlImage, pq.Array(listProductID), stateActive)
 	if err != nil {
 		return nil, err
 	}
@@ -125,6 +137,18 @@ func (r *repository) GetTotalProduct(ctx context.Context) (int64, error) {
 	return total, nil
 }
 
+func (r *repository) GetImagesByProductID(ctx context.Context, productID int64) ([]ProductImage, error) {
+	sql := `SELECT id, product_id, image FROM product_images WHERE product_id=$1`
+
+	var images []ProductImage
+	err := r.db.SelectContext(ctx, &images, sql, productID)
+	if err != nil {
+		return nil, err
+	}
+
+	return images, nil
+}
+
 func (r *repository) UpsertProduct(ctx context.Context, payload ProductPayload) (int64, error) {
 	tx, err := r.db.Begin()
 	if err != nil {
@@ -137,27 +161,19 @@ func (r *repository) UpsertProduct(ctx context.Context, payload ProductPayload) 
 		}
 	}()
 
-	sql := `INSERT INTO product (name, current_price, original_price,url) VALUES 
-		( $1, $2, $3, $4) ON CONFLICT (name) DO UPDATE SET current_price = $2, original_price = $3, url = $4
-		RETURNING id`
-
-	var id int64
-	err = tx.QueryRowContext(ctx, sql, payload.Name, payload.CurrentPrice, payload.OriginalPrice, payload.URL).Scan(&id)
+	productID, err := r.InsertProduct(ctx, tx, payload)
 	if err != nil {
 		return 0, err
 	}
 
-	sqlImages := `INSERT INTO product_images (product_id, image) VALUES ($1, $2)`
-	for _, image := range payload.Images {
-		_, err := tx.ExecContext(ctx, sqlImages, id, image)
-		if err != nil {
-			return 0, err
-		}
+	err = r.insertingImages(ctx, tx, payload, productID)
+	if err != nil {
+		return 0, err
 	}
 
 	sqlHistory := `INSERT INTO price_history(product_id, current_price, original_price) 
 		VALUES ($1, $2, $3)`
-	_, err = tx.ExecContext(ctx, sqlHistory, id, payload.CurrentPrice, payload.OriginalPrice)
+	_, err = tx.ExecContext(ctx, sqlHistory, productID, payload.CurrentPrice, payload.OriginalPrice)
 	if err != nil {
 		return 0, err
 	}
@@ -167,7 +183,70 @@ func (r *repository) UpsertProduct(ctx context.Context, payload ProductPayload) 
 		return 0, err
 	}
 
+	return productID, nil
+}
+
+func (r *repository) insertingImages(ctx context.Context, tx *sql.Tx, payload ProductPayload, productID int64) error {
+	images, err := r.GetImagesByProductID(ctx, productID)
+	if err != nil {
+		return err
+	}
+
+	mapImages := make(map[string]int64)
+	for _, image := range images {
+		mapImages[image.Image] = image.ID
+	}
+
+	for _, image := range payload.Images {
+		if _, ok := mapImages[image]; ok {
+			delete(mapImages, image)
+			continue
+		}
+
+		err = r.AddNewProductImage(ctx, tx, productID, image)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, val := range mapImages {
+		err = r.SoftDeleteProductImage(ctx, tx, val)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (r *repository) AddNewProductImage(ctx context.Context, tx *sql.Tx, productID int64, image string) error {
+	sqlImages := `INSERT INTO product_images (product_id, image, status) VALUES ($1, $2 , $3)`
+	_, err := tx.ExecContext(ctx, sqlImages, productID, image, stateActive)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) InsertProduct(ctx context.Context, tx *sql.Tx, payload ProductPayload) (int64, error) {
+	sql := `INSERT INTO product (name, current_price, original_price,url) VALUES 
+		( $1, $2, $3, $4) ON CONFLICT (name) DO UPDATE SET current_price = $2, original_price = $3, url = $4
+		RETURNING id`
+
+	var id int64
+	err := tx.QueryRowContext(ctx, sql, payload.Name, payload.CurrentPrice, payload.OriginalPrice, payload.URL).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
 	return id, nil
+}
+
+func (r *repository) SoftDeleteProductImage(ctx context.Context, tx *sql.Tx, id int64) error {
+	sqlImages := `UPDATE product_images SET status = $1 WHERE id = $2`
+	_, err := tx.ExecContext(ctx, sqlImages, stateDeleted, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *repository) InsertPriceHistory(ctx context.Context, productID int64, currentPrice int64, originalPrice int64) error {
@@ -195,10 +274,10 @@ func (r *repository) GetLastPriceHistory(ctx context.Context, productID int64, l
 func (r *repository) UpDatabase(ctx context.Context) error {
 	sql := `CREATE TABLE IF NOT EXISTS public.product (
 		id bigserial NOT NULL,
-		"name" varchar NULL,
-		current_price int8 NULL,
-		original_price int8 NULL,
-		updated_at information_schema."time_stamp" NULL DEFAULT now(),
+		"name" varchar NOT NULL,
+		current_price int8 NOT NULL,
+		original_price int8 NOT NULL,
+		updated_at information_schema."time_stamp" NOT NULL DEFAULT now(),
 		url varchar NULL,
 		CONSTRAINT product_pk PRIMARY KEY (id),
 		CONSTRAINT product_un UNIQUE (name)
@@ -210,10 +289,10 @@ func (r *repository) UpDatabase(ctx context.Context) error {
 
 	sql = `CREATE TABLE IF NOT EXISTS public.price_history (
 		id bigserial NOT NULL,
-		product_id int8 NULL,
-		current_price int8 NULL,
-		original_price int8 NULL,
-		updated_at information_schema."time_stamp" NULL DEFAULT now()
+		product_id int8 NOT NULL,
+		current_price int8 NOT NULL,
+		original_price int8 NOT NULL,
+		updated_at information_schema."time_stamp" NOT NULL DEFAULT now()
 	)`
 
 	_, err = r.db.ExecContext(ctx, sql)
@@ -223,8 +302,9 @@ func (r *repository) UpDatabase(ctx context.Context) error {
 
 	sql = `CREATE TABLE IF NOT EXISTS public.product_images (
 		id bigserial NOT NULL,
-		product_id int8 NULL,
-		image varchar NULL,
+		product_id int8 NOT NULL,
+		image varchar NOT NULL,
+		status int NOT NULL,
 		CONSTRAINT product_images_pk PRIMARY KEY (id)
 	)`
 
